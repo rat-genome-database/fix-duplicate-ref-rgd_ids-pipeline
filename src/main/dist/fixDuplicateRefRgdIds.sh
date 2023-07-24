51662666d0ea4e6c00407d25dd76c64cc3b0ef6a
tutajm <mtutaj@mcw.edu>
2023-07-24 16:25:51 -0500

    upgraded build.gradle

--- src/main/dist/fixDuplicateRefRgdIds.sh
+++ src/main/dist/fixDuplicateRefRgdIds.sh
@@ -4,7 +4,7 @@ APP_HOME=/home/rgddata/pipelines/fixDuplicateRefRgdIds
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 EMAIL_LIST=mtutaj@mcw.edu
 if [ "$SERVER" == "REED" ]; then
-  EMAIL_LIST=RGD.Developers@mcw.edu
+  EMAIL_LIST=rgd.devops@mcw.edu
 fi
 
 $APP_HOME/_run.sh > $APP_HOME/run.log  2>&1
